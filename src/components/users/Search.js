81f24a3bf8fc8b47d08860bd61a58a520a9f4a51
ArshdeepRangi <arshdeeprangi@tactica.ca>
2019-06-17 10:40:09 -0500

    changed search component to use useState react component instead of using traditional state

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,54 +1,43 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
-class Search extends Component {
-  state = {
-    text: ''
-  };
-  onChange = e => {
-    /*this is good if we have only one field in the form, if we have more than
-      one, we use the later approach */
-    //this.setState({ text: e.target.value });
-    this.setState({ [e.target.name]: e.target.value });
-  };
-  onSubmit = e => {
+const Search = ({ searchUsers, showClear, clearUsers, setAlert }) => {
+  const [text, setText] = useState('');
+  const onChange = e => setText(e.target.value);
+  const onSubmit = e => {
     e.preventDefault();
-    if (this.state.text === '') {
-      this.props.setAlert('Please enter something to search.', 'danger');
+    if (text === '') {
+      setAlert('Please enter something to search.', 'danger');
     } else {
-      this.props.searchUsers(this.state.text);
-      this.setState({ text: '' });
+      searchUsers(text);
+      setText('');
     }
   };
-  render() {
-    return (
-      <div style={{ margin: '10px 0 50px 0' }}>
-        <form className='form' onSubmit={this.onSubmit}>
-          <input
-            type='text'
-            name='text'
-            placeholder='Search Users...'
-            value={this.state.text}
-            onChange={this.onChange}
-          />
-          <input
-            type='submit'
-            value='Find'
-            className='nes-btn is-warning btn-block'
-          />
-          {this.props.showClear && (
-            <button
-              className='nes-btn is-error btn-block'
-              onClick={this.props.clearUsers}
-            >
-              Clear
-            </button>
-          )}
-        </form>
-      </div>
-    );
-  }
-}
+
+  return (
+    <div style={{ margin: '10px 0 50px 0' }}>
+      <form className='form' onSubmit={onSubmit}>
+        <input
+          type='text'
+          name='text'
+          placeholder='Search Users...'
+          value={text}
+          onChange={onChange}
+        />
+        <input
+          type='submit'
+          value='Find'
+          className='nes-btn is-warning btn-block'
+        />
+        {showClear && (
+          <button className='nes-btn is-error btn-block' onClick={clearUsers}>
+            Clear
+          </button>
+        )}
+      </form>
+    </div>
+  );
+};
 
 export default Search;
 
@@ -58,7 +47,3 @@ Search.propTypes = {
   showClear: PropTypes.bool.isRequired,
   setAlert: PropTypes.func.isRequired
 };
-
-/* to make search functionality work, we need to push the value the user just 
-entered to the main app component so that everything remains centralized.
-*/
