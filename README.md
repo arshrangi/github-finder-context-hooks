bc313f283c5d211db490be833ed4ac691d97708b
Arshdeep Rangi <arshdeeprangi@tactica.ca>
2021-12-25 11:12:30 -0600

    Update ReadMe and add screenshots

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
+# Overview
+
+A React app to search GitHub users. While creating this app learnt various react concepts like hooks, using axios, 
+destructuring code etc.
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
@@ -36,6 +41,17 @@ If you aren’t satisfied with the build tool and configuration choices, you can
 Instead, it will copy all the configuration files and the transitive dependencies (Webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
 
 You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
+
+## Screen Shots
+
+![Home Page](/assets/github_sc/home.png?raw=true "Home Page")
+
+![Users Listing](/assets/github_sc/users_listing.png?raw=true "Users Listing")
+
+![Specific User](/assets/github_sc/user.png?raw=true "Specific User")
+
+![Specific User](/assets/github_sc/user_details.png?raw=true "Specific User")
+
 
 ## Learn More
 
--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -11,6 +11,17 @@ import {
   SET_LOADING
 } from '../types';
 
+let githubClientId;
+let githubClientSecret;
+
+if (process.env.NODE_ENV !== 'production') {
+  githubClientId = process.env.REACT_APP_GITHUB_CLIENT_ID;
+  githubClientSecret = process.env.REACT_APP_GITHUB_CLIENT_SECRET;
+} else {
+  githubClientId = process.env.GITHUB_CLIENT_ID;
+  githubClientSecret = process.env.GITHUB_CLIENT_SECRET;
+}
+
 const GithubState = props => {
   const initialState = {
     users: [],
@@ -26,9 +37,7 @@ const GithubState = props => {
     //this.setState({ loading: true });
     setLoading();
     const response = await axios.get(
-      `https://api.github.com/search/users?q=${text}&client_id=${
-        process.env.REACT_APP_GITHUB_CLIENT_ID
-      }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+      `https://api.github.com/search/users?q=${text}&client_id=${githubClientId}&client_secret=${githubClientSecret}`
     );
 
     //this.setState({ users: response.data.items, loading: false });
@@ -45,9 +54,7 @@ const GithubState = props => {
     //this.setState({ loading: true });
     setLoading();
     const response = await axios.get(
-      `https://api.github.com/users/${username}?client_id=${
-        process.env.REACT_APP_GITHUB_CLIENT_ID
-      }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+      `https://api.github.com/users/${username}?client_id=${githubClientId}&client_secret=${githubClientSecret}`
     );
 
     //this.setState({ user: response.data, loading: false });
@@ -64,9 +71,7 @@ const GithubState = props => {
     //this.setState({ loading: true });
     setLoading();
     const response = await axios.get(
-      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${
-        process.env.REACT_APP_GITHUB_CLIENT_ID
-      }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${githubClientId}&client_secret=${githubClientSecret}`
     );
 
     //this.setState({ repos: response.data, loading: false });
